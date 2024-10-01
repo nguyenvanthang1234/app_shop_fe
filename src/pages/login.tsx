e570711fc96fd42fd18f6dbb4c9d7d74f79aed3e
nguyenvanthang1234 <vanthangthophu1234@gmail.com>
2024-10-01 11:18:32 +0700

    update code login register

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+// ** import Next
+import { NextPage } from 'next'
+import LoginPage from 'src/views/layouts/pages/login'
+
+type TProps = {}
+
+const Login: NextPage<TProps> = () => {
+  return <LoginPage />
+}
+
+export default Login
---
+++ src/pages/register.tsx
@@ -0,0 +1,11 @@
+// ** import Next
+import { NextPage } from 'next'
+import RegisterPage from 'src/views/layouts/pages/register'
+
+type TProps = {}
+
+const Register: NextPage<TProps> = () => {
+  return <RegisterPage />
+}
+
+export default Register
